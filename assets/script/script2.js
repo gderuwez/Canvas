703bd9604060ea9701858b6c857b342a1c88b751
gderuwez <gderuwez@gmail.com>
2018-07-13 14:22:43 +0200

    responsive canvas mod in progress

--- assets/script/script2.js
+++ assets/script/script2.js
@@ -1,33 +1,35 @@
 let d = document.getElementById("canvas2")
 let dtx = d.getContext("2d");
+var X2 = d.width;
+var Y2 = d.height;
 
 dtx.beginPath();
-dtx.moveTo(10, 542);
-dtx.quadraticCurveTo(148, 893, 712, 542);
+dtx.moveTo(X2/(X2/10), Y2/(Y2/542));
+dtx.quadraticCurveTo(X2/(X2/148), Y2/(Y2/893), X2/(X2/712), Y2/(Y2/542));
 dtx.fillStyle = "black";
 dtx.fill();
 
 dtx.beginPath();
 dtx.fillStyle = "black";
-dtx.fillRect(260, 45, 30, 505);
+dtx.fillRect(X2/(X2/260), Y2/(Y2/45), X2/(X2/30), Y2/(Y2/505));
 
 dtx.beginPath();
 dtx.fillStyle = "black";
-dtx.arc(275, 35, 25, 0, 2*Math.PI);
+dtx.arc(X2/(X2/275), Y2/(Y2/35), 25, 0, 2*Math.PI);
 dtx.fill();
 
 dtx.beginPath();
-dtx.moveTo(0, 500);
-dtx.lineTo(240, 500);
-dtx.lineTo(240, 200);
-dtx.quadraticCurveTo(220, 350, 0, 500);
+dtx.moveTo(X2/(X2/0), Y2/(Y2/500));
+dtx.lineTo(X2/(X2/240), Y2/(Y2/500));
+dtx.lineTo(X2/(X2/240), Y2/(Y2/200));
+dtx.quadraticCurveTo(X2/(X2/220), Y2/(Y2/350), X2/(X2/0), Y2/(Y2/500));
 dtx.fillStyle = "black";
 dtx.fill();
 
 dtx.beginPath();
-dtx.moveTo(310, 50);
-dtx.quadraticCurveTo(400, 400, 310, 500);
-dtx.lineTo(660, 500);
-dtx.quadraticCurveTo(620, 300, 310, 50);
+dtx.moveTo(X2/(X2/310), Y2/(Y2/50));
+dtx.quadraticCurveTo(X2/(X2/400), Y2/(Y2/400), X2/(X2/310), Y2/(Y2/500));
+dtx.lineTo(X2/(X2/660), Y2/(Y2/500));
+dtx.quadraticCurveTo(X2/(X2/620), Y2/(Y2/300), X2/(X2/310), Y2/(Y2/50));
 dtx.fillStyle = "black";
 dtx.fill();
